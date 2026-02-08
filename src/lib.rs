ec183847283739cfdc7d9f54e9480ba042ac90c8
avinkrisv <avinkris7@gmail.com>
2026-02-08 03:23:37 -0500

    added lsp

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,25 @@
-use zed_extension_api as zed;
+use zed_extension_api::{self as zed, Command, LanguageServerId, Result, Worktree};
 
 struct RustforceExtension;
 
 impl zed::Extension for RustforceExtension {
     fn new() -> Self {
         Self
+    }
+
+    fn language_server_command(
+        &mut self,
+        language_server_id: &LanguageServerId,
+        _worktree: &Worktree,
+    ) -> Result<Command> {
+        match language_server_id.as_ref() {
+            "apex-lsp" => Ok(Command {
+                command: "/Users/avinkrism4pro/.cargo/bin/apex-lsp".to_string(),
+                args: vec![],
+                env: vec![],
+            }),
+            _ => Err(format!("Unknown language server: {language_server_id}")),
+        }
     }
 }
 
